8c52808e79e635ee60487eca032cbb821dc6eaa5
logan-donaldson <logan.r.donaldson@gmail.com>
2022-05-18 19:48:16 -0400

    fixed warnings

--- src/Album.js
+++ src/Album.js
@@ -12,7 +12,6 @@ import Box from '@mui/material/Box';
 
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Header from './components/Header.js'
