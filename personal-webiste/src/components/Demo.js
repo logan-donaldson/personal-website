d51391a62c3c3206bcae8e0bae504833b7eace21
logan-donaldson <logan.r.donaldson@gmail.com>
2022-05-18 19:28:33 -0400

    test

--- personal-webiste/src/components/Demo.js
+++ personal-webiste/src/components/Demo.js
@@ -5,6 +5,7 @@ export default function Demo() {
 
     return (
         <Box
+        height="100vh"
         display="flex"
         justifyContent="center"
         alignItems="center"
@@ -12,9 +13,8 @@ export default function Demo() {
             bgcolor: '#F8F8F8',
             }}
         >
-        <Box display="flex">
-        <iframe src="https://hf.space/embed/ldonald3/feather-classifier/+" frameBorder="0" height="600" title="Gradio app" class="container p-0 flex-grow space-iframe"></iframe>
-        </Box>
+        <iframe src="https://hf.space/embed/ldonald3/feather-classifier/+" frameBorder="0" height="100%" width="100%" title="Gradio app" class="container p-0 flex-grow space-iframe" allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; document-domain; encrypted-media; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; vr ; wake-lock; xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-downloads"></iframe>
+        {/*<iframe src="https://hf.space/embed/ldonald3/feather-classifier/+" frameBorder="0" height="600" width="900" title="Gradio app" class="container p-0 flex-grow space-iframe"></iframe>*/}
         </Box>
     )
 }
