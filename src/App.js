6dd74b27125e4e24d1a4f283c864431be431568e
logan-donaldson <logan.r.donaldson@gmail.com>
2022-05-21 11:17:09 -0400

    updated resume

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ class App extends Component {
     {name:"nba2k", img:bball, alt:'Photo of an arcade basketball game', title:'NBA 2K Rating Analysis', 
     desc:'Used pandas and Matplotlib to perform a quantitative NBA 2k rating retrospective',
     skills:"Data cleaning, analysis, and visualization; Web scraping",
-    tech:"Python (pandas, Matplotlib)",
+    tech:"Python (pandas, Matplotlib, Beautiful Soup)",
     link:"https://github.com/logan-donaldson/nba2k-exploratory-analysis",
     par:`As professional sports, and in particular basketball, increasingly embrace analytics, the metrics used to assess players' performance become more numerous and complex. It is no longer sufficient to merely consider average points per game when assessing offensive ability or blocked shots when determining defensive prowess. More sophisticated metrics such as true shooting percentage, real adjusted player efficiency rating, and Per 87.5 have been developed in hopes of better quantifying a player's value. Yet, despite their mathematical sophistication, no collection of metrics will provide an objectively correct assessment. Instead of considering numerous complex statistics one could take a reductionist approach by creating a simple metric that sacrifices correctness for ease of use. Indeed, what if instead of developing increasingly advanced metrics we took the opposite approach? What if we sought to distill a professional basketball player's skill level to a single number? What would such a number look like? In fact, such a metric already exists.
 
     The NBA 2K video game franchise has a released a game for each of the last 21 NBA basketball seasons and in each entry every active NBA basketball player is assigned a rating on scale from 1-99, where higher scores correspond to better players. In practice most players receive a score of 60 or above. In this way, each player's skill is summarized into a single number allowing for easy, interesting comparisons.
     
-    In this project I collect and analyze this player rating data which is web scraped using the popular html parser Beautiful Soup. Then using Pandas and Matplotlib I endeavor to reduce the last 21 years of NBA history into simplistic quantitative terms."`
+    In this project I collect and analyze this player rating data which is web scraped using the popular html parser Beautiful Soup. Then using Pandas and Matplotlib I endeavor to reduce the last 21 years of NBA history into simplistic quantitative terms.`
   },
     {name:"sudoku", img:sudoku, alt:'Photo of a sudoku game', title:'Sudoku Solver', 
     desc:'Linear program to automatically solve a given Sudoku game',
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 
-//import Pdf from '../documents/LoganDonaldson_Resume.pdf';
+import Pdf from '../documents/Website_Resume.pdf';
 
 export default function Header() {
 
-  //const onResumeClick = () => {
-  //  window.open(Pdf);
- //}
+  const onResumeClick = () => {
+    window.open(Pdf);
+ }
 
     return (
         <AppBar position="relative">
@@ -27,13 +27,12 @@ export default function Header() {
                     justifyContent="center"
                 >          
                 <Button color="inherit">About Me</Button>
-                <Button color="inherit"> {/*onClick={onResumeClick}*/}Resume</Button>
+                <Button color="inherit" onClick={onResumeClick}>Resume</Button>
                 <Button color="inherit">Articles & Essays</Button>
                 <Button color="inherit">Contact Me</Button>
                 </Stack>              
             </Toolbar>
             </Box>
-        
         </AppBar>
     )
 }
